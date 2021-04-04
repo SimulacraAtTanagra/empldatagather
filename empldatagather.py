86c9196e25bfe262d5f71f184d9a11b5e8906f73
SimulacraAtTanagra <shaneayers1@gmail.com>
2021-04-04 15:04:33 -0400

    updated empldatagather.py

--- empldatagather.py
+++ empldatagather.py
@@ -5,47 +5,36 @@ Created on Tue Feb 25 16:16:58 2020
 @author: sayers
 """
 
-import pandas as pd
 import pyautogui as pig
 from time import sleep
-from datetime import datetime
 import os
-from os import path
-import re
-import numpy as np
-import win32com.client as win32
+from admin import newest
 
-def doit(emplid):
-    paths = 'S:\\downloads\\'
-    for root, dirs, files in os.walk(paths): 
-        for file in files:  
-      
-            # change the extension from '.mp3' to  
-            # the one of your choice. 
-            if file.startswith('ps'): 
-                filepath = str(paths+file)
-                os.rename(f'{emplid}history.xls') 
-"""
-sleep(5)
-enteremplid = (171,307)
 
-search = (39, 427)
+#TODO clean this up using newest
+def doit(paths,emplid):
+    for file in os.listdir(paths): 
+        if file.startswith('ps'): 
+            filepath = os.path.join(paths,file)
+            newpath= os.path.join(paths,f'{emplid}history.xls')
+            os.rename(filepath,newpath) 
 
-download  = (628, 223)
-
-returntostart= (593, 119)
-"""
-for i in employees:
-    emplid = i
+#TODO replace this with something respectable from selenium
+def pig_nav(emplid):
     sleep(5)
-    pig.click(enteremplid)
+    pig.click(enteremplid)  #this has to be defined
     pig.typewrite(i)
     sleep(1)
-    pig.click(search)
+    pig.click(search)   #this has to be defined
     sleep(15)
-    pig.click(download)
+    pig.click(download) #this has to be defined
     sleep(5)
-    try:
-        doit(i)
-    except:
-        pass
+
+#TODO better error handling for this part please
+def main(employees,path):
+    for i in employees:
+        pig_nav(i)
+        try:
+            doit(path,i)
+        except:
+            pass
